5a1545cb7701f99a524dc2c9a89a38b50618ad6b
mstar303 <91958773+mstar303@users.noreply.github.com>
2023-07-20 09:34:14 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -6,3 +6,7 @@ resource "aws_instance" "myec2" {
    ami = "ami-06ca3ca175f37dd66"
    instance_type = "t2.micro"
 }
+resource "aws_instance" "myec22" {
+   ami = "ami-06ca3ca175f37dd66"
+   instance_type = "t2.micro"
+}
